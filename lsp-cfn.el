14bdcde0b0e6770f8214a78f4f75cc135e0147f4
lWarne <laurencewarne@gmail.com>
2022-07-02 11:27:38 +0100

    Fix some lint warnings

--- lsp-cfn.el
+++ lsp-cfn.el
@@ -1,9 +1,9 @@
-;;; lsp-cfn.el --- cfn-lsp-extra setup for lsp-mode -*- lexical-binding: t -*-
+;;; lsp-cfn.el --- LSP integration for cfn-lsp-extra -*- lexical-binding: t -*-
 
 ;; Author: Laurence Warne
 ;; Maintainer: Laurence Warne
 ;; Version: 0.1
-;; Homepage: https://github.com/Laurence Warne/lsp-cfn.el
+;; URL: https://github.com/LaurenceWarne/lsp-cfn.el
 ;; Package-Requires: ((emacs "27.0") (lsp-mode "8.0.0") (yaml-mode "0.0.15"))
 
 ;;; Commentary:
@@ -22,13 +22,21 @@
 ;;; Custom variables
 
 (defgroup lsp-cfn nil
-  "lsp integration for cfn-lsp-extra."
+  "LSP integration for cfn-lsp-extra."
   :prefix "lsp-cfn-"
   :group 'applications)
 
 (defcustom lsp-cfn-verbose nil
   "If non-nil, run cfn-lsp-extra in verbose mode."
   :type 'boolean
+  :group 'lsp-cfn)
+
+(defcustom lsp-cfn-executable "cfn-lsp-extra"
+  "The cfn-lsp-extra executable.
+
+Should be set before the package is loaded, e.g. in the :init
+block of a use-package declaration."
+  :type 'string
   :group 'lsp-cfn)
 
 ;;; Constants
@@ -65,6 +73,8 @@
 
 ;;; Snippet setup
 
+;; Next couple of functions are modified from: https://github.com/AndreaCrotti/yasnippet-snippets
+
 ;;;###autoload
 (defun lsp-cfn-snippets-initialize ()
   "Load the `lsp-cfn-snippets' snippets directory."
@@ -78,8 +88,8 @@
     (yas-load-directory lsp-cfn-snippets-dir t)))
 
 ;;;###autoload
-(eval-after-load 'yasnippet
-  '(lsp-cfn-snippets-initialize))
+(with-eval-after-load 'yasnippet
+  (lsp-cfn-snippets-initialize))
 
 
 ;;; LSP setup
@@ -91,12 +101,11 @@
              '(lsp-cfn-yaml-mode . "cloudformation"))
 
 (lsp-register-client
- (make-lsp-client :new-connection (lsp-stdio-connection "cfn-lsp-extra")
+ (make-lsp-client :new-connection (lsp-stdio-connection lsp-cfn-executable)
                   :activation-fn (lsp-activate-on "cloudformation")
                   :environment-fn (lambda ()
                                     '(("CFN_LSP_EXTRA_VERBOSE" . lsp-cfn-verbose)))
                   :server-id 'cfn-lsp-extra))
-
 
 (provide 'lsp-cfn)
 
