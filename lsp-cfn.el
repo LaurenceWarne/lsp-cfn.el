924808977d7e5c4068ac92eca22b82fdeb4fff86
lWarne <laurencewarne@gmail.com>
2022-07-04 09:49:30 +0100

    Add formal license boilerplate

--- lsp-cfn.el
+++ lsp-cfn.el
@@ -6,6 +6,19 @@
 ;; URL: https://github.com/LaurenceWarne/lsp-cfn.el
 ;; Package-Requires: ((emacs "27.0") (lsp-mode "8.0.0") (yaml-mode "0.0.15"))
 
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; cfn-lsp-extra setup for lsp-mode.  A lot of the mode setup is copied from
